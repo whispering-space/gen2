ccbd7c83eb68f48506e2a9649400cacc2de1e338
erandelax <erandelax@gmail.com>
2023-11-16 11:45:13 +0000

    fix

--- push.sh
+++ push.sh
@@ -1,1 +1,1 @@
-cd source && git add . && git commit -m 'sync' && git push && cd ../.. && git add . && git commit -m 'sync' && git push
+cd source && git add . && git commit -m 'sync' && git push && cd .. && git add . && git commit -m 'sync' && git push
